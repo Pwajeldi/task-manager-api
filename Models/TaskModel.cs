6d443c44f2e20d0945af75e9fa1f16e146731892
Joshua Joshua <pwajeldi900@gmail.com>
2026-03-17 01:58:26 +0100

    Initial code commit - Updated code for models and dbcontext

--- Models/TaskModel.cs
+++ Models/TaskModel.cs
@@ -2,5 +2,12 @@
 {
     public class TaskModel
     {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public string? UserId { get; set; } // Foreign key to users
     }
 }
