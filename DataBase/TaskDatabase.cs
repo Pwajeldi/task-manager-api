5ef22a09c31ecdc041c00fe981c44f37ab47370a
Joshua Joshua <pwajeldi900@gmail.com>
2026-03-17 03:39:10 +0100

    Added database connections and identityrole configuration

--- DataBase/TaskDatabase.cs
+++ DataBase/TaskDatabase.cs
@@ -4,7 +4,7 @@ using Task_Management_App.Models;
 
 namespace Task_Management_App.DataBase
 {
-    public class TaskDatabase: IdentityDbContext
+    public class TaskDatabase: IdentityDbContext<AppUserModel>
     {
         public DbSet<TaskModel> TaskTable { get; set; }
         public TaskDatabase(DbContextOptions<TaskDatabase> options) : base(options)
--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using Task_Management_App.DataBase;
+using Task_Management_App.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<TaskDatabase>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<AppUserModel, IdentityRole>()
+    .AddEntityFrameworkStores<TaskDatabase>();
+
+builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -17,6 +28,7 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
